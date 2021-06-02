993ff84e8ccefd09cab1de69b431952fceaaa63b
Aeroraven <70007256+Aeroraven@users.noreply.github.com>
2021-06-03 01:35:57 +0800

    Add popup window

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <slidebar></slidebar>
     <top-navigation-bar></top-navigation-bar>
     <page-container></page-container>
-    
+    <z-popup-window title="你好世界">这是一个弹出式窗口，可以把组件放这里</z-popup-window>
   </div>
 </template>
 
@@ -11,12 +11,14 @@
 import TopNavigationBar from './components/TopNavigationBar.vue'
 import Slidebar from './components/Slidebar.vue'
 import PageContainer from './components/PageContainer.vue'
+import ZPopupWindow from './components/ZPopupWindow.vue'
 export default {
     name: 'App',
     components: {
         TopNavigationBar,
         Slidebar,
         PageContainer,
+        ZPopupWindow,
     }
 }
 </script>
