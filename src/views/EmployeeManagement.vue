eebb596815c313f6d3d6d52fbe6589665458f003
Aeroraven <1531052461@qq.com>
2021-07-04 11:05:13 +0800

    Integrate employee & ticket modules

---
+++ src/views/EmployeeManagement.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <span class="zms-view-title zms-primary-color">
+            <v-icon color="primary">mdi-database</v-icon> 员工管理
+        </span>
+        <warehouse-container/>
+    </div>
+</template>
+
+<script>
+import WarehouseContainer from '../components/EmployeeComponents/WarehouseContainer.vue'
+
+export default {
+    name: 'WarehouseManagement',
+    created(){
+        this.$store.dispatch('alterTopNavTitle',{title:this.$store.state.sPageName.employeeManagement})
+    },components:{
+        WarehouseContainer,
+    }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
