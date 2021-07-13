3eea5440ccd3e738abcf6689733c57fb492a0c40
Aeroraven <1531052461@qq.com>
2021-07-14 01:05:16 +0800

    调整动物饲喂布局

--- src/components/AnimalFeed/AnimalFeedContainer.vue
+++ src/components/AnimalFeed/AnimalFeedContainer.vue
@@ -1,20 +1,36 @@
 <template>
     <div class="zms-animal-feed">
 
-        <v-container>
-            <v-row>
-                <v-col class="recipe-info" cols=12 sm=6>
+        <!--<v-container>
+            <v-row no-gutters>
+                <v-col class="recipe-info" >
                     <div class="zms-fixed-height">
                         <animal-recipes-info/>
                     </div>
                 </v-col>
-                <v-col class="feed-info" cols=12 sm=6 position:fixed>
+                <v-col class="feed-info" >
                     <div class="zms-fixed-height">
                         <animal-feed-info/>
                     </div>
                 </v-col>
             </v-row>
-        </v-container>
+        </v-container>-->
+        <table>
+            <tr>
+                <td >
+                    <div class="zms-fixed-height">
+                        <animal-recipes-info/>
+                    </div>
+                </td>
+                <td valign="top">
+                    <div class="zms-fixed-height">
+                        <animal-feed-info/>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        
+            
     </div>
 </template>
 
@@ -30,12 +46,21 @@ export default{
    
 }
 </script>
-<style>
+<style scoped lang="scss">
     .zms-fixed-height{
-        position:fixed;
-        width:45%;
+        //position:fixed;
+        width:95%;
+        margin-right:20px;
         bottom: 0px;
         top:95px;
         overflow: scroll;
+        display: inline-block;
+    }
+    table, td,tr{
+        table-layout : fixed
+    }
+    td{
+        width:50%;
+        vertical-align:top;
     }
 </style>
